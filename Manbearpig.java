d19953042680b4a008c064d5b8f7758510d168b1
bruhmoment <40273654+Splsch@users.noreply.github.com>
2020-11-04 22:52:53 -0600

    Added manbearpig
    He's half man, half bear, half pig.

---
+++ Manbearpig.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Manbearpig here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Manbearpig extends Actor
+{
+    /**
+     * Act - do whatever the Manbearpig wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Manbearpig() {
+        setImage(new GreenfootImage("manbearpig.png"));
+    }
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
