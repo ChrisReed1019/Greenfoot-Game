fcc2c5241db1ef109540fe568ced7ea33e0877df
bruhmoment <40273654+Splsch@users.noreply.github.com>
2020-11-04 22:38:35 -0600

    added obsidian

--- Player.java
+++ Player.java
@@ -95,6 +95,7 @@ public class Player extends Actor
                     currTile.type = "grass";
                     MyWorld.scoreBoard.score=0;
                     MyWorld.scoreBoard.update();
+                } else if (currTile.type == "obsidian") {
                     
                 }
             } /* else if("c".equals(key)) {
